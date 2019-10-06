66d49835553505d41f251b8d6cad380cb8735b29
radoslavzi <radoslav.zlatev@gmail.com>
2019-10-06 14:41:05 +0300

    Linux Rado HW

---
+++ Radoslav Ivanov/Линукс и езици за програмиране в биоинформатиката/Week1/factorial.py
@@ -0,0 +1,17 @@
+#defining a function calculating n!
+
+def factorial(num):
+    if num == 1 or num == 0:
+        return 1
+    
+    return num * factorial(num - 1)
+
+print(factorial(5)) #120
+
+# another way using for loop for calculating 10!
+
+prod = 1
+for num in range(1, 11):
+    prod *= num
+
+print(prod)
