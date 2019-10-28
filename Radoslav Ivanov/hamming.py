ad7d6bd314e927de76b90a8be62c6c5c8ae1e9bc
radoslavzi <radoslav.zlatev@gmail.com>
2019-10-29 00:35:26 +0200

    change hamming

--- Radoslav Ivanov/hamming.py
+++ Radoslav Ivanov/hamming.py
@@ -1,5 +1,9 @@
 class Hamming:
     def __init__(self, str1, str2):
+
+        if len(str1) != len(str2):
+            raise AssertionError("Both strings should be same size")
+
         self.str1 = str1
         self.str2 = str2
     
@@ -11,7 +15,11 @@ class Hamming:
 
         return result
 
-hm = Hamming('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT')
+file = open('data/hamming.txt', 'r')
+str1 = file.readline().strip()
+str2 = file.readline().strip()
+file.close()
+hm = Hamming(str1, str2)
 distance = hm.calculateDistance()
 
 print(distance)
