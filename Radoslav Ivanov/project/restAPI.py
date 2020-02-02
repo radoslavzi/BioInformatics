156f6f1a5277270b054acd6a31936d6ded823b40
radoslavzi <radoslav.zlatev@gmail.com>
2020-02-03 00:16:04 +0200

    refactor

--- Radoslav Ivanov/project/restAPI.py
+++ Radoslav Ivanov/project/restAPI.py
@@ -1,29 +1,41 @@
-from flask import Flask
 import requests, sys
-from sample_exam import SequenceBuilder
+from flask import Flask, session, request, send_file
 
 app = Flask(__name__)
+
 SERVER = "https://rest.ensembl.org"
 GENE_ID_EXT = '/sequence/id/'
-#ENSG00000157764
 EXON_EXT = '/lookup/id/'
 
-class EnsembleAPI:
+@app.route('/v1/sequence/gene/id/<id>')
+def gene(id):
+    sequence = requests.get(SERVER + GENE_ID_EXT + id, headers={ "Content-Type" : "application/json"}).json()['seq']
+    exons = requests.get(SERVER + EXON_EXT + id + '?expand=1', headers={ "Content-Type" : "application/json"}).json()
+    responseExons =  buildExonResponse(exons['Transcript'], str(sequence))
+    gcContent = request.args.get('gc_content')
+    swap =  request.args.get('swap')
+    contentType = request.args.get('content-type')
 
-    def __init__(self, seq):
-        self.seq = seq
+    if gcContent:
+        calculateGCContent(swap, seq)
+    else:
+        return  {'seq': sequence, 'exons': responseExons}
 
-    @app.route('/v1/sequence/gene/id/<id>')
-    def gene(self, id):
-       # sequence = requests.get(SERVER + GENE_ID_EXT + id, headers={ "Content-Type" : "application/json"}).json()
-       # exons = requests.get(SERVER + EXON_EXT + id + '?expand=1', headers={ "Content-Type" : "application/json"}).json()
-        #responseExons =  self.buildExonResponse(exons['Exon'], '')
-        return 'dsa'
+def calculateGCContent(swap, seq):
+    replaceDict = {}
+    if swap:
+        replaceDict = dict({swap[0]: swap[2]})
     
-    def buildExonResponse(self, exons, sequence):
-        result = list()
+    return {'seq': seq,
+        'gc_content': seq.count('G') + seq.count('Ç'),
+        'swap_sequence': replace(seq, replaceDict)
+    }
 
-        for exon in exons:
+def buildExonResponse(transcripts, sequence):
+    result = list()
+
+    for trans in transcripts:
+        for exon in trans['Exon']:
             result.append(
                 {'start': exon['start'], 
                 'end': exon['end'],
@@ -31,9 +43,18 @@ class EnsembleAPI:
                 'seq': sequence[exon['start']:exon['end']]
                 })
 
-        return result
+    return result
+   
+def replace(sequence, replaceDict):
+        generatedSeq = ''
 
-    def run(self):
-        app.run(debug=True)
+        for ch in sequence:
+            val = replaceDict.get(ch)
+            if(not(val is None)):
+                generatedSeq += replaceDict.get(ch)
+            else:
+                generatedSeq += ch
 
-EnsembleAPI('seq').run()
+        return generatedSeq
+
+app.run()
